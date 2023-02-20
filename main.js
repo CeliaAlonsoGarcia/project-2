aee376c64ebe70d5fa8706910cc2b0e4b5799377
Celia Alonso <celiaag94@gmail.com>
2023-02-20 20:56:23 +0000

    button has correct js

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 
-  function toggleDisplay(id) {
-    var copy = document.getElementById(id);
-    if (copy.style.display === "none") {
-      copy.style.display = "block";
-    } else {
-      copy.style.display = "none";
-    }
+
+const elements = [
+  { buttonId: "button1", textId: "text1" },
+  { buttonId: "button2", textId: "text2" },
+  { buttonId: "button3", textId: "text3" },
+  { buttonId: "button4", textId: "text4" },
+];
+
+elements.forEach(function(element) {
+  const button = document.querySelector("#" + element.buttonId);
+  const text = document.querySelector("#" + element.textId);
+  let isTextVisible = false;
+
+button.addEventListener("click", function() {
+  if (isTextVisible) {
+    text.style.display = "none";
+    isTextVisible = false;
+  } else {
+    text.style.display = "block";
+    isTextVisible = true;
   }
+});
+});
